94b1f5e8e96e78958eeb287a5c8d4172f7663dd1
RandomAmbersky <random@random.ran>
2016-02-05 19:03:43 +0300

    и удаляем лишнее :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
-var typescript = require('gulp-typescript');
+var babel = require('gulp-babel');
 
-var tsProject = typescript.createProject({});
+//var typescript = require('gulp-typescript');
+//var tsProject = typescript.createProject({});
 
 gulp.task('phaser', function() {
     return gulp
@@ -9,15 +10,35 @@ gulp.task('phaser', function() {
         .pipe(gulp.dest('release/'));
 });
 
-gulp.task('compile-scripts', function() {
-    return gulp
-        .src('lib/*.ts')
-        .pipe(typescript(tsProject))
-        .js.pipe(gulp.dest('release/'));
+var libDir = "lib/";
+var jsFiles = "src/**/*.js";
+
+gulp.task("babel", function() {
+    return gulp.src(jsFiles)
+        .pipe(babel())
+        .pipe(gulp.dest(libDir));
 });
 
-gulp.task('watch', ['compile-scripts'], function () {
-    gulp.watch('lib/*.ts', ['compile-scripts']);
+gulp.task("babel-watch", function() {
+    gulp.watch(jsFiles, ["babel"]);
 });
 
-gulp.task('default', ['phaser', 'compile-scripts']);
+//gulp.task("babel"), function() {
+//	// return gulp.src('lib/*.js')
+//    .pipe(babel())
+//    .pipe(gulp.dest('release'));
+//});
+//gulp.task('compile-scripts', function() {
+    //return gulp
+        //.src('lib/*.ts')
+        //.pipe(typescript(tsProject))
+        //.js.pipe(gulp.dest('release/'));
+//});
+
+gulp.task('watch', ['babel-watch'], function () {
+    gulp.watch(jsFiles, ['babel-watch']);
+});
+
+//gulp.task('default', ['phaser']);
+
+gulp.task('default', ['phaser', 'babel']);
